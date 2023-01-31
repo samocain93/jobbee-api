a3a664ae83f43c316fc8e524023128dc8e09a682
Sam O'Cain <samocain93@gmail.com>
2023-01-31 00:17:26 -0600

    init setup done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config({path : './config/config.env'})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
 })
 
 
