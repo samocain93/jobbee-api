20769fd47d5e1a2284ace5dadff5ad46d73477bf
Sam O'Cain <samocain93@gmail.com>
2023-01-31 00:04:58 -0600

    env set up

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 
+const dotenv = require('dotenv');
+
+
+
+
+// Setting up config.env file variables
+dotenv.config({path : './config/config.env'})
+
 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 })
+
+
